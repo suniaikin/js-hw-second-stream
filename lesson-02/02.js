e358de230c67159a3cccdeabe5d3eff331ea6fb1
suniaikin <class.suniaikin@gmail.com>
2024-12-17 23:05:30 -0800

    lesson02 task02 fixed dice interval

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 7); // your code
-let dice2 = Math.floor(Math.random() * 7); // your code
+let dice1 = Math.floor((Math.random() + 1) * 7); // your code
+let dice2 = Math.floor((Math.random() + 1) * 7); // your code
 let isWinningDouble = dice1 === dice2 && dice1 > 3 && dice2 > 3; // your code
 
 /*
