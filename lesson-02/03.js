79d135646e340675bace8e956f3a47308ea21197
suniaikin <class.suniaikin@gmail.com>
2024-12-20 19:41:28 -0800

    second mistake is fixed

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,25 +1,28 @@
 /*
  * Напишите код, который будет определять оценку студента (grade) в зависимости от балла (score).
- * Балл может быть целым числом от 0 до 100, а оценки 'F' (0 - 49), 'D' (50 - 69), 'C' (70-79), 'B' (80 - 89), 'A' (90 - 100)
+ * Балл может быть целым числом от 0 до 100, а оценки
+ * 'F' (0 - 49),
+ * 'D' (50 - 69),
+ * 'C' (70 - 79),
+ * 'B' (80 - 89),
+ * 'A' (90 - 100)
  * Результат сохраните в переменной grade.
  */
 
-let score = 70; // тестовое значение, можно изменять
+let score = 85; // тестовое значение, можно изменять
 let grade;
 // your code
 
-score = Math.floor(Math.random() * 101);
-
 if (score >= 0 && score < 50) {
-    grade === "F";
+    grade = "F";
 } else if (score >= 50 && score < 70) {
-    grade === "D";
+    grade = "D";
 } else if (score >= 70 && score < 80) {
-    grade === "C";
+    grade = "C";
 } else if (score >= 80 && score < 90) {
-    grade === "B";
+    grade = "B";
 } else {
-    grade === "A";
+    grade = "A";
 }
 
 console.log(`Your score is ${score}, your grade is ${grade}`);
