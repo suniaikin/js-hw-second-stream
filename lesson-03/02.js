aa4490b30812a9dad0ba3e163a3532e7caf195d1
suniaikin <class.suniaikin@gmail.com>
2024-12-28 16:39:41 -0800

    task 03-02

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,15 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+//num is a random number
+function isEven(num) {
+	if (num % 2 === 0) {
+		console.log(true);
+	} else {
+		console.log(false);
+	}
+	return num
+}
+
+console.log(isEven(6));
+
