fd33cba49f1ce1abe253f12e052cec9458e4e916
suniaikin <class.suniaikin@gmail.com>
2024-12-29 01:32:39 -0800

    task 03-03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,21 +1,23 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 // Задаем три переменные для каждого из трех чисел
-let num1 = Math.random() * 4;
-let num2 = Math.random() * 4;
-let num3 = Math.random() * 4;
+// let num1 = Math.random() * 4;
+// let num2 = Math.random() * 4;
+// let num3 = Math.random() * 4;
+
+let num1 = Math.floor(Math.random() * 4);
+let num2 = Math.floor(Math.random() * 4);
+let num3 = Math.floor(Math.random() * 4);
 
 function findLargest(num1, num2, num3){
-    if (num1 > num2 && num1 > num3) {
+    if (num1 >= num2 && num1 >= num3) {
         return num1;
-    } else if (num2 > num1 && num2 > num3){
+    } else if (num2 >= num1 && num2 >= num3){
         return num2;
-    } else if (num3 > num2 && num3 > num1){
+    } else if (num3 >= num2 && num3 >= num1){
         return num3;
-    } else if (num1 > num2 && num1 < num3) {
-
     } else {
-        return "Не определено"
+        return num1
     }
 }
 
