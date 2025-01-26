badcafcdfa2d807ec0cab1128599176864773241
suniaikin <class.suniaikin@gmail.com>
2025-01-25 23:54:35 -0800

    task 04-03 fix length

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,13 +17,15 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-let arrayFirst = [1, 2, 8, 4, 5, 3, 7, 8, 2, 10, 1, 12]
-let arraySecond = [11, 32, 8, 24, 55, 33, 77, 88, 2, 10, 14, 122]
+let arrayFirst = [1, 2, 3 ]
+let arraySecond = [2, 3, 4]
 
 function findCommonElements(array1, array2) {
     let arrayCommonElements = []
       for (let i = 0; i < array1.length; i++) {
-        if(array1[i] === array2[i]) {
+        if(array1.includes(array2[i])) {
+            arrayCommonElements.push(array2[i]);
+        } else if ((array2.includes(array1[i])) && (!arrayCommonElements.includes(array1[i]))) {
             arrayCommonElements.push(array1[i]);
         }
 
