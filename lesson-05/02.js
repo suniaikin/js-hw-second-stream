ba6a243bbf7d9cb426461f56fa8385a3a9170fb7
Андрей Суняйкин <145228881+nau5292@users.noreply.github.com>
2025-02-07 12:17:27 -0800

    2nd task

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -30,11 +30,21 @@ console.log(gallery)
 }
 */
 
+
 const gallery = {
   'Mona Lisa': 'Leonardo da Vinci',
   'Starry Night': 'Vincent van Gogh',
   'The Scream': 'Edvard Munch',
   
+
+
+function updateGallery(gallery, pictureTitle, value) {
+  gallery[pictureTitle] ;
 }
 
-function updateGallery(gallery) {}
+updateGallery(gallery, "Mona Lisa", "Leonardo da Vinci, 1503-1506");
+
+
+updateGallery(gallery, "The Persistence of Memory", "Salvador Dali");
+
+console.log(gallery);
