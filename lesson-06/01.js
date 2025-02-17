30ea722a1f84511b90d43c116c9daf98e2de06ce
suniaikin <class.suniaikin@gmail.com>
2025-02-17 09:41:53 -0800

    init

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,30 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+
+const imageElement = document.querySelector('#web-tech-image');
+const prevButton = document.querySelector('#prev-button');
+const nextButton = document.querySelector('#next-button');
+
+let currentIndex = 0;
+
+// Функция для обновления изображения
+function updateImage() {
+  imageElement.src = WEB_TECH_IMAGES[currentIndex];
+}
+
+// Обработчик для кнопки "prev"
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+  updateImage();
+});
+
+// Обработчик для кнопки "next"
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % WEB_TECH_IMAGES.length;
+  updateImage();
+});
+
+// Устанавливаем начальное изображение
+updateImage();
