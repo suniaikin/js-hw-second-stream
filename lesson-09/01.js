39874fc96e19a6998054247e544052c9e8a9a0ea
suniaikin <class.suniaikin@gmail.com>
2025-03-05 14:33:24 -0800

    init

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -23,4 +23,12 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = (array, callback) => {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i)) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+};
